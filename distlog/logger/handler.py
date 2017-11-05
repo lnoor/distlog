60932d15694aab3f35f2bac3047799ba9de5ef22
lnoor <leo@noordergraaf.net>
2017-11-05 21:44:34 +0100

    add 0MQ logging handler

---
+++ distlog/logger/handler.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Publish log messages via 0MQ.
+
+The LogRecord contents are, once properly formatter,
+sent out using 0MQ.
+
+"""
+
+__copyright__ = "Copyright (C) 2017 Leo Noordergraaf"
+__licence__ = "GNU General Public Licence v3"
+
+import logging
+
+import zmq
+from zmq.utils.strtypes import cast_bytes
+
+
+TOPIC_SEPARATOR = ''
+TOPIC_SYSTEM = 'TSP'
+TOPIC_ENCODING = 'JP'
+TOPIC_LOGGING = 'L'
+TOPIC_PERFORMANCE = 'P'
+"""Message topics.
+
+Message topics are used to describe the contents of the
+next fragment. It consists of three parts SYSTEM, KIND
+and ENCODING. The parts are separated by the TOPIC_SEPARATOR.
+
+A client subscribes to these topics and only receives those
+messages to which it subscribed.
+
+The SYSTEM topic is one of the letters TSP which stand for:
+
+(T)est
+    The test environment.
+    The systems used for unit and integration testing.
+
+(S)taging
+    The staging (acceptation) environment.
+    The systems used for functional and acceptance testing.
+
+(P)roduction
+    The production environment.
+    The place where the software becomes the cash cow.
+
+The KIND defines the kind of message:
+
+(L)og message
+    An (extended) LogRecord.
+    A regular python logging message.
+
+(P)erformance message
+    A special kind of message.
+    These messages contain data on performance subjects suitable for
+    display in some fancy dashboard application. Since these messages
+    share most if not all the requirements of normal log messages it
+    makes sense to let them share the infrastructure.
+
+Finally the ENCODING defines how the message is transferred:
+
+(J)SON
+    The LogRecord structure is encoded using JSON.
+    This allows non python programs to generate or process the messages.
+
+(P)ickle
+    Python's own serialization method.
+    Python specific but smaller and faster than JSON.
+"""
+
+
+class ZmqPUBHandler(logging.Handler):
+
+    """0MQ transport implementation."""
+
+    log_topic = None
+    perf_topic = None
+    socket = None
+    context = None
+
+    def __init__(self, endpoint, context=None):
+        """Create a ZmqPUBHandler.
+
+        This creates the 0MQ PUB socket and connects its with an
+        endpoint.
+        :param string endpoint: A 0MQ endpoint like `tcp://localhost:11223`.
+        :param socket endpoint: An endpoint can also be a connected socket.
+        :param context: A ZMQ context.
+
+        """
+        super().__init__()
+        if isinstance(endpoint, zmq.Socket):
+            self.socket = endpoint
+            self.context = self.socket.context
+        else:
+            self.context = context or zmq.Context.instance()
+            self.socket = self.context.socket(zmq.PUB)
+            self.socket.connect(endpoint)
+
+    def set_topic(self, system, encoding):
+        """Set message topic elements.
+
+        Creates the topic strings for log and performance messages.
+        :param system: the system topic
+        :param encoding: the encoding topic
+
+        """
+        assert system in TOPIC_SYSTEM
+        assert encoding in TOPIC_ENCODING
+
+        self.log_topic = TOPIC_SEPARATOR.join(
+            [system, TOPIC_LOGGING, encoding]
+        )
+
+    def emit(self, record):
+        """Do whatever it takes to actually log the specified logging record.
+        """
+        try:
+            bmsg = cast_bytes(self.format(record))
+            btopic = cast_bytes(self.log_topic)
+        except Exception:
+            self.handleError(record)
+            return
+        self.socket.send_multipart([btopic, bmsg])
+
+#    def send_perf_data(self, data):
+#        bdata = cast_bytes(data)
+#        btopic = cast_bytes(self.perf_topic)
+#
+#        self.socket.send_multipart([btopic, bdata])
---
+++ tests/test_handler.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+import json
+from threading import Thread
+
+import zmq
+from zmq.utils.strtypes import cast_unicode
+
+try:
+    from distlog.logger.handler import ZmqPUBHandler
+    from distlog.logger.formatters import JSONFormatter
+except:
+    import sys
+    sys.path.append('..')
+    from distlog.logger.handler import ZmqPUBHandler
+    from distlog.logger.formatters import JSONFormatter
+
+
+CONNECTPOINT = "tcp://localhost:6000"
+BINDPOINT = "tcp://*:6000"
+
+def publisher_thread():
+    handler = ZmqPUBHandler(CONNECTPOINT,  zmq.Context())
+    handler.set_topic('T', 'J')
+    handler.setFormatter(JSONFormatter())
+
+    time.sleep(1)
+
+    for i in range(10):
+        record = logging.LogRecord('name', 20, '/here/and/nowhere/else.py', 50, 'hi there %s %d', ('number', 1), None)
+        handler.emit(record)
+    handler = None
+
+def subscriber_thread():
+    ctx = zmq.Context()
+    subscriber = ctx.socket(zmq.SUB)
+    subscriber.subscribe('')
+    subscriber.bind(BINDPOINT)
+
+    count = 0
+    while True:
+        if count == 10:
+            break
+
+        msg = subscriber.recv_multipart()
+        assert msg[0] == b'TLJ'
+        data = json.loads(cast_unicode(msg[1]))
+        assert data['name'] == 'name'
+        assert data['levelno'] == 20
+        assert data['filename'] == 'else.py'
+        assert data['module'] == 'else'
+        assert data['lineno'] == 50
+        assert data['message'] == 'hi there number 1'
+        count += 1
+
+
+def test_handler():
+    s_thread = Thread(target=subscriber_thread)
+    p_thread = Thread(target=publisher_thread)
+    s_thread.start()
+    p_thread.start()
+
+    s_thread.join()
+    p_thread.join()
+
+if __name__ == '__main__':
+    test_handler()
