51c924dc531591b7c8f6519135f44234436dcfeb
lnoor <leo@noordergraaf.net>
2018-03-07 13:34:01 +0100

    fixed typo

--- distlog/__init__.py
+++ distlog/__init__.py
@@ -2,4 +2,4 @@ from .logger.context import import_task, task, to
 from .logger.formatters import JSONFormatter, PickleFormatter
 from .logger.handler import ZmqHandler
 
-__all__ = ['import_task', 'task', 'to', 'JSONFormatter', 'PickleFormatter', 'ZmgHandler']
+__all__ = ['import_task', 'task', 'to', 'JSONFormatter', 'PickleFormatter', 'ZmqHandler']
